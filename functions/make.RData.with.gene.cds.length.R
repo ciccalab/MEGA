45734fca7f8dd586e5d2927e00c3e24b906d171b
jeky82 <gennaro.gmb@gmail.com>
2017-02-01 11:45:11 +0100

    fixed bug in the computation of the cds length.

--- functions/make.RData.with.gene.cds.length.R
+++ functions/make.RData.with.gene.cds.length.R
@@ -7,7 +7,7 @@ get.cds.length = function(TxDb)
   cds.loc.all <- cds.loc.all[map.entrez.symbol$ENTREZID]
   
   # get cds length for the remaning genes (entrez ids)
-  tmp <- do.call("rbind",lapply(cds.loc.all, function(x) data.frame(cds_id = x@elementMetadata$cds_id[which.max(x@ranges@width)], width = x@ranges@width[which.max(x@ranges@width)],stringsAsFactors = F)))
+  tmp <- do.call("rbind",lapply(cds.loc.all, function(x) data.frame(cds_id = x@elementMetadata$cds_id[which.max(x@ranges@width)], width = sum(x@ranges@width),stringsAsFactors = F)))
   
   # make final data frame and return it
   gene.cds.length <- cbind(map.entrez.symbol,tmp)
@@ -18,7 +18,7 @@ get.cds.length = function(TxDb)
 # HG19
 require(Homo.sapiens)
 require(TxDb.Hsapiens.UCSC.hg19.knownGene)
-wd <- "~/MEGA/"
+wd <- "~/mnt/nirvana_home/MEGA_git/"
 setwd(wd)
 gene.cds.length <- get.cds.length(TxDb.Hsapiens.UCSC.hg19.knownGene)
 save(gene.cds.length,file = "./RData/HG19.gene.cds.length.RData")
@@ -26,7 +26,7 @@ save(gene.cds.length,file = "./RData/HG19.gene.cds.length.RData")
 # HG38
 require(Homo.sapiens)
 require(TxDb.Hsapiens.UCSC.hg38.knownGene)
-wd <- "~/MEGA/"
+wd <- "~/mnt/nirvana_home/MEGA_git/"
 setwd(wd)
 gene.cds.length <- get.cds.length(TxDb.Hsapiens.UCSC.hg38.knownGene)
 save(gene.cds.length,file = "./RData/HG38.gene.cds.length.RData")
